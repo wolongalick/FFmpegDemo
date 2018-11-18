2d9692d0b97317e2fb7ec5671e66019d6cce69e4
wolongalick <1607009565@qq.com>
2018-11-18 21:52:17 +0800

    初步集成完毕ffmpeg,可以调用ffmpeg的api

---
+++ app/src/main/java/com/alick/ffmpegdemo/FFmpegPlayer.java
@@ -0,0 +1,18 @@
+package com.alick.ffmpegdemo;
+
+public class FFmpegPlayer {
+    static {
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avfilter-6");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("swscale-4");
+        System.loadLibrary("cxw_ffmpeg");
+
+//        System.loadLibrary("postproc-54");
+//        System.loadLibrary("avdevice-57");
+    }
+
+    public native void playMyMedia(String url);
+}
